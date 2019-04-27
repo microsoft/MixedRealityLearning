9ae7b3a72e87c943094a11abc68b3bc7b5894bce
mr0ng <sean.ong@outlook.com>
2019-04-26 20:23:05 -0700

    ASA Lessons progress + updates to Base Module Asset Pack

---
+++ MixedRealityBase/Assets/BaseModuleAssets/Base Module Scripts/MoveWithPan.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class MoveWithPan : MonoBehaviour, IMixedRealityHandPanHandler
+{
+
+
+    private void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update ()
+    {
+	}
+
+    public void OnPanEnded(HandPanEventData eventData)
+    {
+
+    }
+
+    public void OnPanning(HandPanEventData eventData)
+    {
+        Vector3 panningPosition = new Vector3(eventData.PanPosition.x, eventData.PanPosition.y , 0.0f);
+        transform.localPosition += panningPosition;
+
+
+    }
+
+    public void OnPanStarted(HandPanEventData eventData)
+    {
+
+    }
+}
