0cb13cd34516648bcfea364ffc39f202165f5e4a
Martin Wilter <martinwilter@gmail.com>
2020-02-12 14:54:09 -0800

    Asa: Assets: Fix: Script field name and...
    ...the need to remove TMP placeholder text from scene.

--- MRTK Tutorials/Assets/MRTK.Tutorials.AzureSpatialAnchors/Scripts/AnchorFeedbackScript.cs
+++ MRTK Tutorials/Assets/MRTK.Tutorials.AzureSpatialAnchors/Scripts/AnchorFeedbackScript.cs
@@ -7,7 +7,7 @@ public class AnchorFeedbackScript : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Reference to the Text Mesh Pro component on this object.")]
-    private TextMeshPro textMeshProObject = default;
+    private TextMeshPro feedbackText = default;
 
     private AnchorModuleScript anchorModuleScript;
 
@@ -29,48 +29,53 @@ public class AnchorFeedbackScript : MonoBehaviour
         anchorModuleScript.OnASAAnchorLocated += AnchorModuleScript_OnASAAnchorLocated;
     }
 
+    void Start()
+    {
+        feedbackText.text = "";    
+    }
+
     private void AnchorModuleScript_OnStartASASession()
     {
-        textMeshProObject.text = " Starting Azure session";
+        feedbackText.text = " Starting Azure session";
     }
 
     private void AnchorModuleScript_OnEndASASession()
     {
-        textMeshProObject.text = "Ending Azure session";
+        feedbackText.text = "Ending Azure session";
     }
 
     private void AnchorModuleScript_OnCreateAnchorStarted()
     {
-        textMeshProObject.text = "Creating Azure anchor";
+        feedbackText.text = "Creating Azure anchor";
     }
 
     private void AnchorModuleScript_OnCreateAnchorSucceeded()
     {
-        textMeshProObject.text = "Azure anchor creation succeeded";
+        feedbackText.text = "Azure anchor creation succeeded";
     }
 
     private void AnchorModuleScript_OnCreateAnchorFailed()
     {
-        textMeshProObject.text = "Azure anchor creation failed";
+        feedbackText.text = "Azure anchor creation failed";
     }
 
     private void AnchorModuleScript_OnCreateLocalAnchor()
     {
-        textMeshProObject.text = "Creating local anchor";
+        feedbackText.text = "Creating local anchor";
     }
 
     private void AnchorModuleScript_OnRemoveLocalAnchor()
     {
-        textMeshProObject.text = "Removing local anchor";
+        feedbackText.text = "Removing local anchor";
     }
 
     private void AnchorModuleScript_OnFindASAAnchor()
     {
-        textMeshProObject.text = "Trying to find Azure anchor";
+        feedbackText.text = "Trying to find Azure anchor";
     }
 
     private void AnchorModuleScript_OnASAAnchorLocated()
     {
-        textMeshProObject.text = "Azure anchor located";
+        feedbackText.text = "Azure anchor located";
     }
 }
