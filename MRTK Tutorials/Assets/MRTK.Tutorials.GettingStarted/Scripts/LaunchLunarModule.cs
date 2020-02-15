b057c3f8f0c990927fd9bb7bd65ad22095406ef7
Martin Wilter <martinwilter@gmail.com>
2020-02-14 16:59:07 -0800

    Base4: Importing the tutorial assets
    MRTK.HoloLens2.Unity.Tutorials.Assets.GettingStarted.2.2.0.1.unitypackage

---
+++ MRTK Tutorials/Assets/MRTK.Tutorials.GettingStarted/Scripts/LaunchLunarModule.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchLunarModule : MonoBehaviour
+{
+    public float thrust;
+    Rigidbody rb;
+    bool ThrustOn;
+
+    private Vector3 originalPosition;
+    private Quaternion originalRotation;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        originalPosition = transform.position;
+        originalRotation = transform.rotation;
+    }
+
+    public void ResetModule()
+    {
+        StopThruster();
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
+        transform.position = originalPosition;
+        transform.rotation = originalRotation;
+    }
+
+    public void StartThruster()
+    {
+        StartCoroutine(Thruster());
+    }
+
+    public void StopThruster()
+    {
+        ThrustOn = false;
+    }
+
+    public IEnumerator Thruster()
+    {
+        rb.isKinematic = false;
+
+        ThrustOn = true;
+
+        yield return null;
+
+        while (ThrustOn)
+        {
+            yield return new WaitForSeconds(0.01f);
+            rb.AddForce(transform.up * thrust);
+        }
+    }
+}
---
+++ MRTK Tutorials/Assets/MRTK.Tutorials.GettingStarted/Scripts/LoadOnClick.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using Microsoft.MixedReality.Toolkit;
+
+public class LoadOnClick : MonoBehaviour
+{
+
+    public GameObject GameobjectToDisable;
+
+    // Use this for initialization
+    void Awake()
+    {
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    public void LoadScene(string levelname)
+    {
+               
+        StartCoroutine(LoadSceneCoRoutine(levelname));
+    }
+
+    IEnumerator LoadSceneCoRoutine(string levelname)
+    {
+
+        SceneManager.LoadScene("BlankTransitionScene");
+        yield return new WaitForSeconds(0.1f);
+
+        var mrtkps = GameObject.Find("MixedRealityPlayspace");
+        var mrtk = GameObject.Find("MixedRealityToolkit");
+
+        //If it finds the wrong mrtk object then destroy it and look a second time
+        if (mrtk.GetComponent<MixedRealityToolkit>() == null)
+        {
+            Destroy(mrtk);
+            mrtk = GameObject.Find("MixedRealityToolkit");
+        }
+
+        GameobjectToDisable.SetActive(false);
+
+        if (mrtkps != null)
+        {
+            Debug.Log("Destroying MRTK Playspace");
+            Destroy(mrtkps);
+        }
+
+        if (mrtk != null)
+        {
+            Debug.Log("Destroying MRTK");
+            Destroy(mrtk);
+        }
+
+        var objects = GameObject.FindObjectsOfType<GameObject>();
+        foreach (GameObject o in objects)
+        {
+            if (o != gameObject || o.GetComponentInParent<LoadOnClick>())
+                Destroy(o);
+        }
+
+        yield return new WaitForSeconds(5f);
+
+        SceneManager.LoadScene(levelname);
+
+        yield return new WaitForSeconds(0.1f);
+
+        GameobjectToDisable.SetActive(true);
+    }
+}
