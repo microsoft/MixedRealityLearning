d2e9d843005825a9daef5e79015aaa0cd034908d
Martin Wilter <martinwilter@gmail.com>
2020-04-07 11:32:35 -0700

    Sharing: Assets: Fix: Script: Eliminate need to overwrite script

---
+++ MRTK Tutorials/Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/SharingModuleScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharingModuleScript : MonoBehaviour
+{
+    AnchorModuleScript anchorModuleScript;
+
+    void Start()
+    {
+        anchorModuleScript = GetComponent<AnchorModuleScript>();
+    }
+
+    public void ShareAnchorNetwork()
+    {
+        DebugWindowMessaging.Clear();
+        Debug.Log("\nSharingModuleScript.ShareAnchorNetwork()");
+
+        GenericNetworkManager.instance.AzureAnchorID = anchorModuleScript.currentAzureAnchorID;
+        Debug.Log("GenericNetworkManager.AzureAnchorID: " + GenericNetworkManager.instance.AzureAnchorID);
+
+        GameObject PVuser = GenericNetworkManager.instance.localUser.gameObject;
+        PhotonUser pu = PVuser.gameObject.GetComponent<PhotonUser>();
+        pu.PVShareAnchorNetwork();
+    }
+
+    public void GetSharedAnchorNetwork()
+    {
+        DebugWindowMessaging.Clear();
+        Debug.Log("\nSharingModuleScript.GetSharedAnchorNetwork()");
+        Debug.Log("GenericNetworkManager.AzureAnchorID: " + GenericNetworkManager.instance.AzureAnchorID);
+
+        anchorModuleScript.FindAzureAnchor(GenericNetworkManager.instance.AzureAnchorID);
+    }
+}
