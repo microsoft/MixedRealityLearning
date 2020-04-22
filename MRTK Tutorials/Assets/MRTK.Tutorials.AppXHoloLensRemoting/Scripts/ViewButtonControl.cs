3cbea04f40f96be0ed2109f940efc6d1a74e796b
skyshadow-ar <60536542+skyshadow-ar@users.noreply.github.com>
2020-04-22 10:19:45 +0530

    3dViewer Completed

---
+++ MRTK Tutorials/Assets/MRTK.Tutorials.AppXHoloLensRemoting/Scripts/ViewButtonControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewButtonControl : MonoBehaviour
+{
+    public GameObject[] Models;
+    private int ModelIndex = 0;
+    
+    void Start()
+    {
+      Models[0].SetActive(true);
+    }
+
+    public void NextModel()
+    {
+        Models[ModelIndex].SetActive(false);
+        ModelIndex = ModelIndex + 1;
+        if(ModelIndex < Models.Length)
+        {
+            Models[ModelIndex].SetActive(true);
+        }
+        else
+        {
+            ModelIndex = 0;
+            Models[ModelIndex].SetActive(true);
+        }
+        
+        
+    }
+
+    public void PreviousModel()
+    {
+        
+        Models[ModelIndex].SetActive(false);
+        ModelIndex = ModelIndex - 1;
+        if(ModelIndex >= 0)
+        {
+            Models[ModelIndex].SetActive(true);
+        }
+        else
+        {
+            ModelIndex = Models.Length-1;
+            Models[ModelIndex].SetActive(true);
+
+        }
+        
+    }
+}
