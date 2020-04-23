f1d91d210cfb99e052b9c00481322d51cba987fa
paul <paul,gumpert@onginnovations.com>
2020-04-23 14:37:58 -0700

    Script: cleaning RoverController and ExplodedView

--- MRTK Tutorials/Assets/_Paul/Scripts/RoverController.cs
+++ MRTK Tutorials/Assets/_Paul/Scripts/RoverController.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class RoverController : MonoBehaviour
 {
-    [SerializeField] float driveSpeed = 10f;
-    [SerializeField] float driveFactor = 1f;
-    [SerializeField] float turnFactor = 100f;
-    [SerializeField] float movementLimit = .5f;
+    [Header("Joystick refrence")]
     [SerializeField] GameObject joystick;
+    [Header("Rover refrences")]
     [SerializeField] Rigidbody rigidBody;
     [SerializeField] Animator animator;
-
-    bool isAtStartingPosition = false;
+    [Header("Movement settings")]
+    [Tooltip("The speed that the wheels spin")] [SerializeField] float driveSpeed = 10f;
+    [Tooltip("The speed that the rover moves")] [SerializeField] float driveFactor = 10f;
+    [Tooltip("The speed that the rover turns")] [SerializeField] float turnFactor = 100f;
+    
     Vector3 startJoystickPos;
     Vector3 oldJoystickPos;
 
+    float movementLimit = .1f;
+    bool isAtStartingPosition = false;
+
     void Start()
     {
+        // get the starting position for the joystick
         startJoystickPos = joystick.transform.localPosition;
-        //rigidBody = GetComponent<Rigidbody>();
-        //animator = GetComponent<Animator>();
         animator.SetBool("isDriving", false);
     }
-    
+
     void Update()
     {
-        
-        //joystick.transform.localPosition = new Vector3(joystick.transform.localPosition.x, 0, joystick.transform.localPosition.z);
+        // move joystick back to original position after manipulation has ended
         if (!isAtStartingPosition)
         {
             joystick.transform.localPosition = Vector3.Lerp(joystick.transform.localPosition, startJoystickPos, .1f);
@@ -36,24 +38,28 @@ public class RoverController : MonoBehaviour
                 isAtStartingPosition = true;
             }
         }
+        // lock the position of the joystick on the y axis
         if (joystick.transform.localPosition.y != startJoystickPos.y)
         {
             joystick.transform.localPosition = new Vector3(joystick.transform.localPosition.x, startJoystickPos.y, joystick.transform.localPosition.z);
         }
+        // keep the joystick from moving further than the movementLimit
         if (Vector3.Distance(transform.position, joystick.transform.position) > movementLimit)
         {
             joystick.transform.localPosition = oldJoystickPos;
         }
-
+        // track the last position of the joystick
         oldJoystickPos = joystick.transform.localPosition;
-        Debug.Log("oldjoystickPos " + oldJoystickPos);
+
+        // move the rover based of the position of the joystick
         float translation = oldJoystickPos.z * 10f;
         float rotation = (oldJoystickPos.x * 10f) * turnFactor;
-        Debug.Log("translation = " + translation);
-        animator.SetFloat("DrivingSpeed", translation * driveSpeed);
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
         Quaternion turn = Quaternion.Euler(0, rotation, 0);
+        animator.SetFloat("TurnFactor", rotation);
+
+        // if the rover is driving in backwards reverse the turn direction
         if (translation < 0)
         {
             turn = Quaternion.Euler(0, -rotation, 0);
@@ -61,11 +67,12 @@ public class RoverController : MonoBehaviour
 
         rigidBody.MoveRotation(rigidBody.rotation * turn);
         rigidBody.transform.Translate(Vector3.forward * translation);
-        animator.SetFloat("TurnFactor", rotation);
+        animator.SetFloat("DrivingSpeed", translation * driveSpeed);
 
+        // start the driving animation if we are moving
         if (translation != 0)
         {
-            animator.SetBool("isDriving", true);         
+            animator.SetBool("isDriving", true);
         }
         else
         {
@@ -73,14 +80,8 @@ public class RoverController : MonoBehaviour
         }
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, movementLimit);
-    }    
-
     public void EndManipulation()
     {
-        isAtStartingPosition = false;        
+        isAtStartingPosition = false;
     }
 }
