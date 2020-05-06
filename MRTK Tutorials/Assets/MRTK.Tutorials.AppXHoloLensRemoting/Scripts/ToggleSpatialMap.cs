3001018c1f8f6fc3d82b2c155d02478df74cf37b
Varun Kumar Siddaraju <31942847+varunsiddaraju@users.noreply.github.com>
2020-05-06 12:31:51 -0700

    Added spatial map hand menu feature

---
+++ MRTK Tutorials/Assets/MRTK.Tutorials.AppXHoloLensRemoting/Scripts/ToggleSpatialMap.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System.Linq;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using UnityEngine;
+
+public class ToggleSpatialMap : MonoBehaviour
+{
+    public void ToggleSpatialMaps()
+    {
+        if (CoreServices.SpatialAwarenessSystem != null)
+        {
+            if (IsObserverRunning)
+            {
+                CoreServices.SpatialAwarenessSystem.SuspendObservers();
+                CoreServices.SpatialAwarenessSystem.ClearObservations();
+            }
+            else
+            {
+                CoreServices.SpatialAwarenessSystem.ResumeObservers();
+            }
+        }
+    }
+
+    private bool IsObserverRunning
+    {
+        get
+        {
+            var providers =
+                ((IMixedRealityDataProviderAccess)CoreServices.SpatialAwarenessSystem)
+                .GetDataProviders<IMixedRealitySpatialAwarenessObserver>();
+            return providers.FirstOrDefault()?.IsRunning == true;
+        }
+    }
+}
