5de9c056ff879946090c68861d7f6722317f7103
Martin Wilter <martinwilter@gmail.com>
2020-05-25 20:03:42 -0700

    Feat: UX for create object location

---
+++ Azure Cloud Tutorials/Assets/_WIP/Martin/Scripts/AnchorIndicator.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+namespace MRTK.Tutorials.AzureCloudPower
+{
+	/// <summary>
+	/// TODO: 
+	/// </summary>
+	[RequireComponent(typeof(TapToPlace), typeof(SphereCollider))]
+	public class AnchorIndicator : MonoBehaviour
+	{
+		[SerializeField]
+		bool allowSpeechCommand = true;
+
+		AnchorManager anchorManager;
+		Interactable interactableVisual;
+
+		void OnEnable()
+		{
+			// Reset TapToPlace any time the game object is enabled
+			ResetTapToPlace();
+		}
+
+		void Start()
+		{
+			// I'm enabled by my manager when I'm needed
+			gameObject.SetActive(false);
+
+			// Chache references
+			anchorManager = GetComponentInParent<AnchorManager>();
+			interactableVisual = GetComponentInChildren<Interactable>();
+
+			// Set collider size to control TaptoPlace.SurfaceNormalOffset (offset = radius)
+			GetComponent<SphereCollider>().radius = 0;
+
+			// Configure Interactable
+			if (allowSpeechCommand)
+			{
+				interactableVisual.OnClick.AddListener(OnPlacingStoppedAndOnClickHandler);
+			}
+			else
+			{
+				interactableVisual.IsEnabled = false;
+			}
+		}
+
+		void ResetTapToPlace()
+		{
+			// Set up layer mask array
+			LayerMask layerMask = LayerMask.GetMask("Spatial Awarenes");
+			int layerNumber = (int)(Mathf.Log((uint)layerMask.value, 2));
+			LayerMask[] layerMasks = { layerNumber };
+
+			// Remove and add (required for AutoStart = true)
+			Destroy(GetComponent<TapToPlace>());
+			TapToPlace tapToPlace = gameObject.AddComponent(typeof(TapToPlace)) as TapToPlace;
+
+			// Configure
+			tapToPlace.AutoStart = true;
+			tapToPlace.DefaultPlacementDistance = 1;
+			tapToPlace.MaxRaycastDistance = 3;
+			tapToPlace.SurfaceNormalOffset = 0;
+			tapToPlace.KeepOrientationVertical = true;
+			tapToPlace.RotateAccordingToSurface = false;
+			tapToPlace.MagneticSurfaces = layerMasks;
+
+			// Subscribe to event
+			tapToPlace.OnPlacingStopped.AddListener(OnPlacingStoppedAndOnClickHandler);
+		}
+
+		void OnPlacingStoppedAndOnClickHandler()
+		{
+			// Tell manager to create anchor at my current position
+			anchorManager.Create();
+
+			// Disable myself because I'm no longer needed
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Azure Cloud Tutorials/Assets/_WIP/Martin/Scripts/AnchorManager.cs
@@ -0,0 +1,66 @@
+#define DEVELOP
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+
+namespace MRTK.Tutorials.AzureCloudPower
+{
+	/// <summary>
+	/// TODO: 
+	/// </summary>
+#if !DEVELOP
+	[RequireComponent(typeof(SpatialAnchorManager))]
+#endif
+	public class AnchorManager : MonoBehaviour
+	{
+		[SerializeField]
+		GameObject anchorIndicator = default;
+		[SerializeField]
+		GameObject anchorPositionPrefab = default;
+
+		// Called from dialog button 'Yes'
+		public void StartPlacingAnchor()
+		{
+			Debug.Log("__\nAnchorManager.EnableAnchorIndicator()");
+
+			// Enable AnchorIndicator (triggers TapToPlace with AutoStart = true)
+			anchorIndicator.SetActive(true);
+		}
+
+		public void Create()
+		{
+			Debug.Log("__\nAnchorManager.Create()");
+
+			AppManager_OnAnchorCreated();
+		}
+
+		#region TODO: TEMP APP MANAGER
+		[SerializeField]
+		GameObject objectCard = default;
+
+		void AppManager_OnAnchorCreated()
+		{
+			Debug.Log("__\nAnchorManager.AppManager_OnAnchorCreated()");
+
+			#region TODO: TEMP UX MANAGER
+			// When the anchor is created, place a visual at the anchor position...
+			Instantiate(anchorPositionPrefab, anchorIndicator.transform.position, anchorIndicator.transform.rotation);
+
+			// ...and disable the the object card
+			objectCard.SetActive(false);
+			#endregion
+		}
+
+		public void AppManager_OnAnchorClicked()
+		{
+			Debug.Log("__\nAnchorManager.APPManager_OnAnchorClicked()");
+
+			#region TODO: TEMP UX MANAGER
+			// Enable the object card because user clicked anchor
+			objectCard.SetActive(true);
+			#endregion
+		}
+		#endregion
+	}
+}
