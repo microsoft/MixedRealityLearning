318ba08f177281bce87d408517865a2931d506eb
Ruben de la Torre <ruben@studio-delatorre.com>
2020-06-04 21:11:14 +0200

    Add remaining Azure Table and Blob Storage features.

--- Azure Cloud Tutorials/Assets/_WIP/Ruben/Scripts/Managers/DataManager.cs
+++ Azure Cloud Tutorials/Assets/_WIP/Ruben/Scripts/Managers/DataManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage.Table;
 using MRTK.Tutorials.AzureCloudPower.Domain;
 using UnityEngine;
@@ -29,6 +31,8 @@ namespace MRTK.Tutorials.AzureCloudPower.Managers
         private CloudStorageAccount storageAccount;
         private CloudTableClient cloudTableClient;
         private CloudTable trackedObjectsTable;
+        private CloudBlobClient blobClient;
+        private CloudBlobContainer blobContainer;
 
         private async void Awake()
         {
@@ -50,6 +54,24 @@ namespace MRTK.Tutorials.AzureCloudPower.Managers
                     Debug.LogException(ex);
                 }
             }
+
+            blobClient = storageAccount.CreateCloudBlobClient();
+            blobContainer = blobClient.GetContainerReference(blockBlobContainerName);
+            if (tryCreateBlobContainerOnStart)
+            {
+                try
+                {
+                    if (await blobContainer.CreateIfNotExistsAsync())
+                    {
+                        Debug.Log($"Created container {blockBlobContainerName}.");
+                    }
+                }
+                catch (StorageException ex)
+                {
+                    Debug.LogError("Failed to connect with Azure Storage.\nIf you are running with the default storage emulator configuration, please make sure you have started the storage emulator.");
+                    Debug.LogException(ex);
+                }
+            }
         }
 
         /// <summary>
@@ -63,6 +85,18 @@ namespace MRTK.Tutorials.AzureCloudPower.Managers
             var result = await trackedObjectsTable.ExecuteAsync(insertOrMergeOperation);
 
             return result.HttpStatusCode == (int)HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Get all TrackObject from the table.
+        /// </summary>
+        /// <returns>List of TrackedObject from table.</returns>
+        public async Task<List<TrackedObject>> GetAllTrackedObjects()
+        {
+            var query = new TableQuery<TrackedObject>();
+            var segment = await trackedObjectsTable.ExecuteQuerySegmentedAsync(query, null);
+
+            return segment.Results;
         }
 
         /// <summary>
@@ -94,6 +128,61 @@ namespace MRTK.Tutorials.AzureCloudPower.Managers
             var segment = await trackedObjectsTable.ExecuteQuerySegmentedAsync(query, null);
 
             return segment.Results.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Delete a TrackedObject from the table.
+        /// </summary>
+        /// <param name="instance">Object to delete.</param>
+        /// <returns>Success result of deletion.</returns>
+        public async Task<bool> DeleteTrackedObject(TrackedObject instance)
+        {
+            var deleteOperation = TableOperation.Delete(instance);
+            var result = await trackedObjectsTable.ExecuteAsync(deleteOperation);
+
+            return result.HttpStatusCode == (int)HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Upload data to a blob.
+        /// </summary>
+        /// <param name="data">Data to upload.</param>
+        /// <param name="blobName">Name of the blob, ideally with file type.</param>
+        /// <returns>Uri to the blob.</returns>
+        public async Task<string> UploadBlob(byte[] data, string blobName)
+        {
+            var blockBlob = blobContainer.GetBlockBlobReference(blobName);
+            await blockBlob.UploadFromByteArrayAsync(data, 0, data.Length);
+
+            return blockBlob.Uri.ToString();
+        }
+
+        /// <summary>
+        /// Download data from a blob.
+        /// </summary>
+        /// <param name="blobName">Name of the blob.</param>
+        /// <returns>Data as byte array.</returns>
+        public async Task<byte[]> DownloadBlob(string blobName)
+        {
+            var blockBlob = blobContainer.GetBlockBlobReference(blobName);
+            await blockBlob.FetchAttributesAsync();
+            var data = new byte[blockBlob.Properties.Length];
+            await blockBlob.DownloadToByteArrayAsync(data, data.Length);
+
+            return data;
+        }
+
+        /// <summary>
+        /// Delete a blob if it exists.
+        /// </summary>
+        /// <param name="blobName">Name of the blob to delete.</param>
+        /// <returns>Success result of deletion.</returns>
+        public async Task<bool> DeleteBlob(string blobName)
+        {
+            var blockBlob = blobContainer.GetBlockBlobReference(blobName);
+            await blockBlob.DeleteIfExistsAsync();
+
+            return blockBlob.IsDeleted;
         }
     }
 }
