6bdbfaac6e5d0089694a6fd526fa034d50e9be95
Vinayak <vinay@veeruby.com>
2020-06-06 11:13:15 +0530

    Imported PC holographic pakage

---
+++ PC Holographic Remoting/PC Holographic Remoting/Assets/MRTK.Tutorials.PCHolographicRemoting/Scripts/ToggleButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleButton : MonoBehaviour
+{
+    public GameObject ClipingObject;
+    public void ToggleCliping()
+    {
+        ClipingObject.SetActive(!ClipingObject.activeInHierarchy);
+    }
+}
