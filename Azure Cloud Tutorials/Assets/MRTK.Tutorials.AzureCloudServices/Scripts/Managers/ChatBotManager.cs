216a08273e495a5064afc50278a0782e20a1db56
Ruben de la Torre <ruben@studio-delatorre.com>
2020-06-17 21:27:27 +0200

    Add initial chatbot manager class and some minor adaptions on some prefabs.

---
+++ Azure Cloud Tutorials/Assets/MRTK.Tutorials.AzureCloudServices/Scripts/Managers/ChatBotManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace MRTK.Tutorials.AzureCloudServices.Scripts.Managers
+{
+    public class ChatBotManager : MonoBehaviour
+    {
+        public bool IsListening { get; private set; }
+        
+        [SerializeField]
+        private SceneController sceneController;
+        [SerializeField]
+        private string botWebResource;
+        
+        private void Awake()
+        {
+            if (sceneController == null)
+            {
+                sceneController = FindObjectOfType<SceneController>();
+            }
+        }
+
+        public void StartConversation()
+        {
+            if (IsListening)
+            {
+                return;
+            }
+            
+            Debug.Log("Starting conversation with Bot.");
+            IsListening = true;
+        }
+    }
+}
