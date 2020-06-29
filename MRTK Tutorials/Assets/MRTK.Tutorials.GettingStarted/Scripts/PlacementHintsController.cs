210b3889cbc5919c9eb1ee30f44a4733a02e428d
Martin Wilter <martinwilter@gmail.com>
2020-06-28 18:08:16 -0700

    Base: Assets: Scripts: Update for Sharing changes

--- MRTK Tutorials/Assets/MRTK.Tutorials.GettingStarted/Scripts/PlacementHintsController.cs
+++ MRTK Tutorials/Assets/MRTK.Tutorials.GettingStarted/Scripts/PlacementHintsController.cs
@@ -4,8 +4,17 @@ namespace MRTK.Tutorials.GettingStarted
 {
     public class PlacementHintsController : MonoBehaviour
     {
+        public delegate void PlacementHintsControllerDelegate();
+
         [SerializeField] private bool activeAtStart = true;
         [SerializeField] private GameObject[] placementHints = default;
+
+        private bool isPunEnabled;
+
+        public bool IsPunEnabled
+        {
+            set => isPunEnabled = value;
+        }
 
         private void Start()
         {
@@ -13,10 +22,30 @@ namespace MRTK.Tutorials.GettingStarted
             foreach (var obj in placementHints) obj.SetActive(activeAtStart);
         }
 
+        /// <summary>
+        /// Triggers the placement hints feature.
+        /// Hooked up in Unity.
+        /// </summary>
         public void TogglePlacementHints()
+        {
+            if (isPunEnabled)
+                OnTogglePlacementHints?.Invoke();
+            else
+                Toggle();
+        }
+
+        /// <summary>
+        /// Toggles the placement hints' active state.
+        /// </summary>
+        public void Toggle()
         {
             // Toggle each object's active state
             foreach (var obj in placementHints) obj.SetActive(!obj.activeSelf);
         }
+
+        /// <summary>
+        ///     Raised when TogglePlacementHints is called and PUN is enabled.
+        /// </summary>
+        public event PlacementHintsControllerDelegate OnTogglePlacementHints;
     }
 }
