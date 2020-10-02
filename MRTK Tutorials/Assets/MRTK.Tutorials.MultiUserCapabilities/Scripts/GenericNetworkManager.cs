6c7b4f7320718d19251cdd91d779948d968fd2ab
Martin Wilter <martinwilter@gmail.com>
2020-10-02 15:35:51 -0700

    Fix: Not possible to merge into default branch
    Added all from default branch.

---
+++ MRTK Tutorials/Assets/MRTK.Tutorials.MultiUserCapabilities/Scripts/GenericNetworkManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+
+namespace MRTK.Tutorials.MultiUserCapabilities
+{
+    public class GenericNetworkManager : MonoBehaviour
+    {
+        public static GenericNetworkManager Instance;
+
+        [HideInInspector] public string azureAnchorId = "";
+        [HideInInspector] public PhotonView localUser;
+        private bool isConnected;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                if (Instance != this)
+                {
+                    Destroy(Instance.gameObject);
+                    Instance = this;
+                }
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start()
+        {
+            ConnectToNetwork();
+        }
+
+        // For future non PUN solutions
+        private void StartNetwork(string ipAddress, string port)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ConnectToNetwork()
+        {
+            OnReadyToStartNetwork?.Invoke();
+        }
+
+        public static event Action OnReadyToStartNetwork;
+    }
+}
