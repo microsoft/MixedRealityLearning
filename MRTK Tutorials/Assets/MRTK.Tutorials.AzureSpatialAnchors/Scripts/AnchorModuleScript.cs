d077d44feb14f84259298ab713089c3d519af401
Brandon Phillips <8829544+bphillips09@users.noreply.github.com>
2021-07-12 18:21:43 -0500

    Update ASA/MRTK to use UPM (2.7.2, 2.10.1-beta 1)

--- MRTK Tutorials/Assets/MRTK.Tutorials.AzureSpatialAnchors/Scripts/AnchorModuleScript.cs
+++ MRTK Tutorials/Assets/MRTK.Tutorials.AzureSpatialAnchors/Scripts/AnchorModuleScript.cs
@@ -125,7 +125,7 @@ public class AnchorModuleScript : MonoBehaviour
         CloudSpatialAnchor localCloudAnchor = new CloudSpatialAnchor();
 
         // Now we set the local cloud anchor's position to the native XR anchor's position
-        localCloudAnchor.LocalAnchor = theObject.FindNativeAnchor().GetPointer();
+        localCloudAnchor.LocalAnchor = await theObject.FindNativeAnchor().GetPointer();
 
         // Check to see if we got the local XR anchor pointer
         if (localCloudAnchor.LocalAnchor == IntPtr.Zero)
