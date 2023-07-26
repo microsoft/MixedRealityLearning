029eab5ed29d0213dd4d70f35fe30923153a48ce
Marlena Klein <125413876+marlenaklein-msft@users.noreply.github.com>
2023-07-25 17:00:19 -0700

    ASA tutorial (#70)
    Updated ASA Tutorial

---
+++ MRTK3 Tutorials/Assets/MRTK.Tutorials.AzureSpatialAnchors/Scripts/AnchorFeedbackScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AnchorFeedbackScript : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Reference to the Text Mesh Pro component on this object.")]
+    private TextMeshPro feedbackText = default;
+
+    private AnchorModuleScript anchorModuleScript;
+
+    void Awake()
+    {
+        anchorModuleScript = GameObject.FindObjectOfType<AnchorModuleScript>();
+
+        anchorModuleScript.OnStartASASession += AnchorModuleScript_OnStartASASession;
+        anchorModuleScript.OnEndASASession += AnchorModuleScript_OnEndASASession;
+
+        anchorModuleScript.OnCreateAnchorStarted += AnchorModuleScript_OnCreateAnchorStarted;
+        anchorModuleScript.OnCreateAnchorSucceeded += AnchorModuleScript_OnCreateAnchorSucceeded;
+        anchorModuleScript.OnCreateAnchorFailed += AnchorModuleScript_OnCreateAnchorFailed;
+
+        anchorModuleScript.OnCreateLocalAnchor += AnchorModuleScript_OnCreateLocalAnchor;
+        anchorModuleScript.OnRemoveLocalAnchor += AnchorModuleScript_OnRemoveLocalAnchor;
+
+        anchorModuleScript.OnFindASAAnchor += AnchorModuleScript_OnFindASAAnchor;
+        anchorModuleScript.OnASAAnchorLocated += AnchorModuleScript_OnASAAnchorLocated;
+    }
+
+    void Start()
+    {
+        feedbackText.text = "";    
+    }
+
+    private void AnchorModuleScript_OnStartASASession()
+    {
+        feedbackText.text = " Starting Azure session";
+    }
+
+    private void AnchorModuleScript_OnEndASASession()
+    {
+        feedbackText.text = "Ending Azure session";
+    }
+
+    private void AnchorModuleScript_OnCreateAnchorStarted()
+    {
+        feedbackText.text = "Creating Azure anchor";
+    }
+
+    private void AnchorModuleScript_OnCreateAnchorSucceeded()
+    {
+        feedbackText.text = "Azure anchor creation succeeded";
+    }
+
+    private void AnchorModuleScript_OnCreateAnchorFailed()
+    {
+        feedbackText.text = "Azure anchor creation failed";
+    }
+
+    private void AnchorModuleScript_OnCreateLocalAnchor()
+    {
+        feedbackText.text = "Creating local anchor";
+    }
+
+    private void AnchorModuleScript_OnRemoveLocalAnchor()
+    {
+        feedbackText.text = "Removing local anchor";
+    }
+
+    private void AnchorModuleScript_OnFindASAAnchor()
+    {
+        feedbackText.text = "Trying to find Azure anchor";
+    }
+
+    private void AnchorModuleScript_OnASAAnchorLocated()
+    {
+        feedbackText.text = "Azure anchor located";
+    }
+}
---
+++ MRTK3 Tutorials/Assets/MRTK.Tutorials.AzureSpatialAnchors/Scripts/DisableDiagnosticsSystem.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+
+namespace MRTK.Tutorials.AzureSpatialAnchors
+{
+    /// <summary>
+    ///     If enabled, disables DiagnosticsSystem when running on Android or iOS.
+    /// </summary>
+    public class DisableDiagnosticsSystem : MonoBehaviour
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        [SerializeField] [Header("Android & iOS Settings")]
+        private bool disableDiagnosticsSystem = true;
+
+        private void Start()
+        {
+            if (disableDiagnosticsSystem) CoreServices.DiagnosticsSystem.ShowDiagnostics = false;
+        }
+#endif
+    }
+}
