14e4f588207e84b72ddf326ef69f2873e33e14e7
Marlena Klein <125413876+marlenaklein-msft@users.noreply.github.com>
2023-08-22 16:03:45 -0700

    Merge in HolographicRemoting (#74)

--- MRTK3 Tutorials/Assets/MRTK.Tutorials.PCHolographicRemoting/Scripts/ViewButtonControl.cs
+++ MRTK3 Tutorials/Assets/MRTK.Tutorials.PCHolographicRemoting/Scripts/ViewButtonControl.cs
@@ -1,8 +1,7 @@
-﻿// Copyright (c) Microsoft Corporation. 
+﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ViewButtonControl : MonoBehaviour
